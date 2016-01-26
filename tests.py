fada9dc825af9182e55b055519666eeab6d5c9b3
dominikus1993 <dominikus1910@gmail.com>
2016-01-26 23:39:55 +0100

    add range method and test

--- tests.py
+++ tests.py
@@ -1,2 +1,12 @@
+from unittest import TestCase
 import y
 
+
+class Tests(TestCase):
+    def test_range(self):
+        start = 1
+        end = 10
+        test_result = y.range(start, end)
+        self.assertEqual(len(test_result), 10)
+        self.assertEqual(test_result[-1], 10)
+        self.assertEqual(test_result[0], 1)
--- y.py
+++ y.py
@@ -1,5 +1,23 @@
-print("Test")
+from typing import List
+
+def range(start: int, end: int) -> List[int]:
+    def gen(start, end):
+        while start <= end:
+            yield start
+            start += 1
+
+    if start < end:
+        return list(gen(start, end))
+    else:
+        raise Exception("Invalid Parameter Error")
 
 
-def filter():
-    return 2
+def each(collection):
+    return 0
+
+
+def filter(collection, predicate):
+    result = []
+    return result
+
+
